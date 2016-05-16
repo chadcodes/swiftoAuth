502dbbfa9ad9fdb18e63ccbd35aa67c4a8a72284
Chad <chadcodes@users.noreply.github.com>
2016-05-16 10:24:17 -0400

    Started new project, added cartfile for SwiftyJSON and Alamofire

--- oAuth/ViewController.swift
+++ oAuth/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    // a URL must be of type NSURL with one parameter but will need to be unwrapped
+    let loginURL = NSURL(string: "https://www.google.com")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView.delegate = self
+        
+        // use if let to unwrap a potential nil value so we can handle it correctly
+        if let loginURL = loginURL {
+            // we're good
+            print("url was good, continue...")
+            webView.loadRequest(NSURLRequest(URL: loginURL))
+        } else {
+            // error
+            print("url failed")
+        }
+    }
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) ->
+        var continueToUrl = true
+        do {
+            let 
+        } catch {
+        
+        }
+        
+        return
     }
 
     override func didReceiveMemoryWarning() {
